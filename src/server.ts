04063cd4dd6a0191a57b040b4400270ef2d695dc
Harman Kaur <hkaur138@academic.rrc.ca>
2026-02-05 20:20:44 -0600

    fix server file

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import app from "./app";
+import { Server } from "http";
 
-const PORT = Number(process.env.PORT) || 3000;
+const PORT: string | number = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const server: Server = app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
+
+export { server };
